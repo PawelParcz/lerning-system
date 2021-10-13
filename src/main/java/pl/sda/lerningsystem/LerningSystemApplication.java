a2340e0e1580c6c37eda788f28c9ac8f73d9812b
Pawel Parcz <pawelparcz@gmail.com>
2021-10-13 17:56:09 +0200

    Initial commit

---
+++ src/main/java/pl/sda/lerningsystem/LerningSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.lerningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LerningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/lerningsystem/LerningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.lerningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
