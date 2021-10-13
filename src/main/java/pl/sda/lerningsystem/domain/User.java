8da4c46dec109a8e5f66683d57cb6e04f0c748a4
Mateusz K <mateusz.kupiec.pl@gmail.com>
2021-10-13 18:22:49 +0200

    added user entity

---
+++ src/main/java/pl/sda/lerningsystem/domain/User.java
@@ -0,0 +1,34 @@
+package pl.sda.lerningsystem.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
