15837b9c0a4b47cd69a186798c76b23ed1041a9e
Pawel Parcz <pawelparcz@gmail.com>
2021-10-13 18:23:05 +0200

    dodanit widoku, dodanie controlera

---
+++ src/main/java/pl/sda/lerningsystem/controller/UserController.java
@@ -0,0 +1,10 @@
+package pl.sda.lerningsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/useer")
+public class UserController {
+
+}
