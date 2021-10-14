d750a7ba90c6d1670a21345b1590d03df304fc27
Mateusz K <mateusz.kupiec.pl@gmail.com>
2021-10-14 18:13:47 +0200

    added detail view of user

--- src/main/java/pl/sda/lerningsystem/controller/UserController.java
+++ src/main/java/pl/sda/lerningsystem/controller/UserController.java
@@ -2,28 +2,24 @@ package pl.sda.lerningsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.lerningsystem.entity.User;
 import pl.sda.lerningsystem.repository.UserRepository;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     UserRepository repository;
+
     public UserController(UserRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/list")
-    public String getViewUsers(Model model) {
-        List<User> list = repository.findAll();
-        model.addAttribute("users", list);
-        return "listUser";
+    @GetMapping("/{id}")
+    public String getViewUsers(@PathVariable("id") Long id, Model model) {
+        User user = repository.getById(id);
+        model.addAttribute("user", user);
+        return "user";
     }
 
     @GetMapping("/add")
